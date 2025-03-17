b907a9ec150e5d7875583c3941b481534063d36c
Hank Hu <hankhuwm@gmail.com>
2025-03-17 19:52:08 +0800

    本地端11403171952

---
+++ DataAccess/Entities/Club.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Club
+    {
+        [Key]
+        public int ClubId { get; set; }
+        public required string Name { get; set; }
+        public required Sport Sport { get; set; }
+
+
+    }
+}
