b72f547577ae51cd67f7a4059921fab5ba9a9e4e
Hank Hu <hankhuwm@gmail.com>
2025-07-24 09:43:11 +0800

    11407240943_更新: 移除ENTITY不必要的USING

--- Data/Entities/Enrollment.cs
+++ Data/Entities/Enrollment.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SportGroups.Data.Entities
 {
     public class Enrollment   // 活動報名
--- Data/Entities/Message.cs
+++ Data/Entities/Message.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SportGroups.Data.Entities
 {
     public class Message   // 社團訊息
