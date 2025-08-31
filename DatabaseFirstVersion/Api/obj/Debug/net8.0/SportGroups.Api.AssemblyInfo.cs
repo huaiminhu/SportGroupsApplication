1e4f8a6f6eb661559f7f279e4012da775aa6474e
Hank Hu <hankhuwm@gmail.com>
2025-08-31 14:42:51 +0800

    11408311443更新: Docs(Data Tables)

--- DatabaseFirstVersion/Api/obj/Debug/net8.0/SportGroups.Api.AssemblyInfo.cs
+++ DatabaseFirstVersion/Api/obj/Debug/net8.0/SportGroups.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportGroups.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b055940a725cc3b29840a5c2c3f0de37f6713fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b809ff536561def94f65f2962f4bdd1d8df8b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportGroups.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportGroups.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
