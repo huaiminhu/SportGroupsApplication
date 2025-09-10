ce5c8ecd4d1d6d6a4fdac984fa67a961fb5ff8d0
Hank Hu <hankhuwm@gmail.com>
2025-09-10 19:59:05 +0800

    11409102000更新: 刪除SP X3 (Docs X1, DATA/SQL X2), 更新SP X1(DATA/SQL X1)

--- DatabaseFirstVersion/Data/SQL/usp_GetArticleWithCondition.sql
+++ DatabaseFirstVersion/Data/SQL/usp_GetArticleWithCondition.sql
@@ -4,14 +4,15 @@ GO
 CREATE PROCEDURE usp_GetArticleWithCondition
 	@ClubId INT = NULL, 
 	@Sport INT = NULL, 
-	@Keyword NVARCHAR(50)
+	@Keyword NVARCHAR(50) = NULL
 AS
 BEGIN
-	SELECT a.*, m.MediaId, m.MediaFileName, m.MediaType, m.AddedDate FROM Articles a
+	SELECT a.*, m.MediaId, m.MediaFileName, m.MediaType, m.FileUrl, m.AddedDate FROM Articles a
 	LEFT JOIN Medias m
 	ON a.ArticleId = m.ArticleId
 	WHERE (a.ClubId = @ClubId OR @ClubId IS NOT NULL)
 	AND (a.Sport = @Sport OR @Sport IS NOT NULL)
 	AND (a.Title LIKE '%' + @Keyword + '%' OR @Keyword IS NOT NULL)
+	GROUP BY a.ArticleId
 	ORDER BY a.EditedDate DESC;
 END;
--- DatabaseFirstVersion/Data/obj/Debug/net8.0/SportGroups.Data.AssemblyInfo.cs
+++ DatabaseFirstVersion/Data/obj/Debug/net8.0/SportGroups.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportGroups.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc63db54c2e79518855400549ed6116316421d88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70af55b3ab87e2c190f4fba9ced9e2a62720581c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportGroups.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportGroups.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
